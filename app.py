9f152a8f7c8271ff3d654aa322fbabd9ccd80afa
mixqoz <unknownmixqo@gmail.com>
2026-03-20 08:45:38 +0100

    Added navbar template

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/quiz')
 def quiz():
     return render_template('quiz.html')
 
